134b9f4b5b37cf01050a25810c7f65f0299476da
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-03-17 20:47:28 +0530

    create log out button

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  void _logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut(); // Sign out user
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {},
-          child: const Text("Home"),
-        ),
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text("Welcome to Safe Pulse!"),
       ),
     );
   }
