cd33022e1f2a880cd37de65eb20efe33c5485a3f
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-03-23 00:38:44 +0530

    complete bottom navbar

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -25,8 +26,38 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text("Welcome to Safe Pulse!"),
+      bottomNavigationBar: Container(
+        color: Colors.blue,
+        child: const  Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+            gap: 8,
+            backgroundColor: Colors.blue,
+            color: Colors.black,
+            activeColor: Colors.white,
+            //tabBackgroundColor: Colors.red,
+            padding:  EdgeInsets.all(9),
+            tabs:  [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.map,
+                text: 'Map',
+              ),
+              GButton(
+                icon: Icons.add,
+                text: 'Add contact',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+              
+            ],
+            ),
+        ),
       ),
     );
   }
