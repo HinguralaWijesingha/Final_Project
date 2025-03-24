a8e962d86fee9d3de71822807cbc065022e3a0ea
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-03-25 01:12:30 +0530

    add dependencies

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
+
 class ContactPage extends StatelessWidget {
-  const ContactPage({super.key});
+  const ContactPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
