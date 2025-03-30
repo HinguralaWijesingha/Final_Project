901fc583d7b7c46c97a7ca7808bcea3c8ba428b1
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-03-30 20:27:26 +0530

    create police emergency page

--- lib/pages/widgets/police_emergency.dart
+++ lib/pages/widgets/police_emergency.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class PoliceContact extends StatelessWidget {
   const PoliceContact({Key? key}) : super(key: key);
@@ -15,6 +17,72 @@ class PoliceContact extends StatelessWidget {
         child: Container(
           height: 180,
           width: MediaQuery.of(context).size.width * 0.7,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 12, 161, 69),
+                Color.fromARGB(255, 94, 171, 125),
+                Color.fromARGB(255, 112, 229, 143),
+              ],
+            ),
+          ),
+          child:Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.white.withOpacity(0.5),
+                  child: Image.asset("assets/po.png"),
+                ),
+                 Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                       Text(
+                        "Emergency Stiuation",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).size.width * 0.06,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        ),
+                        Text(
+                        "Call 1-1-9 an Emergency Stuation",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize:MediaQuery.of(context).size.width * 0.04
+                        ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '1-1-9',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize:MediaQuery.of(context).size.width * 0.050
+                              ),
+                            ),
+                          ),
+                        )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
