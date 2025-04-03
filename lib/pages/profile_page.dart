2e08f0d83f3ef98a2ebace4ec9b0560f2e03efec
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-04-03 23:31:03 +0530

    get userr data from firebasefirestore

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -14,70 +15,78 @@ class _ProfilePageState extends State<ProfilePage> {
   final currentUser = FirebaseAuth.instance.currentUser!;
 
   //edit
-  Future<void> edit(String field) async {
-
-  }
-
+  Future<void> edit(String field) async {}
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(
-        children:  [
-          const SizedBox(height: 40,),
-
-          const Icon(
-            Icons.person,
-            size: 70,
-            color: Colors.black,
-          ),
-
-          const SizedBox(height: 10,),
-
-          Text(
-            currentUser.email!,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              color: Colors.black
-          ),
-          ),
-
-          const SizedBox(height: 30,),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Text(
-              'My Details',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
-              )
-            ),
-            ),
-
-            ProfileText(
-              text: "Anji", 
-              subText: "User Name",
-              onPressed: () => edit("name"),
-            ),
-
-            ProfileText(
-              text: "imalshanwiesinghe@gmail.com", 
-              subText: "Email",
-              onPressed: () => edit("name"),
-            ),
-
-            ProfileText(
-              text: "0123456789", 
-              subText: "Phone Number",
-              onPressed: () => edit("name"),
-            ),
-
-
-        ],
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('Users')
+            .doc(currentUser.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          //get users data
+          if (snapshot.hasData) {
+            final userData = snapshot.data!.data() as Map<String, dynamic>?;
+            return ListView(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                const Icon(
+                  Icons.person,
+                  size: 70,
+                  color: Colors.black,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  currentUser.email!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.black),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: Text('My Details',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600],
+                      )),
+                ),
+                ProfileText(
+                  text: userData!['name'],
+                  subText: "User Name",
+                  onPressed: () => edit("name"),
+                ),
+                ProfileText(
+                  text: currentUser.email!,
+                  subText: "Email",
+                  onPressed: () => edit("name"),
+                ),
+                ProfileText(
+                  text: userData['phonenumber'],
+                  subText: "Phone Number",
+                  onPressed: () => edit("name"),
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error${snapshot.error}'),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
-     // },
+      // },
     );
   }
 }
