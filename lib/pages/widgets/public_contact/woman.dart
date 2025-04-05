92f977bde6bfba6d16d3144cc21c371c41b04d19
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-04-05 23:03:39 +0530

    create new public contact

---
+++ lib/pages/widgets/public_contact/woman.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+
+class Ambulance extends StatelessWidget {
+  const Ambulance({Key? key}) : super(key: key);
+
+  _callNumber(String number) async{
+  await FlutterPhoneDirectCaller.callNumber(number);
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, bottom: 5),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: InkWell(
+          onTap: () => _callNumber('119'),
+          child: Container(
+            height: 180,
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 12, 161, 69),
+                  Color.fromARGB(255, 94, 171, 125),
+                  Color.fromARGB(255, 112, 229, 143),
+                ],
+              ),
+            ),
+            child:Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white.withOpacity(0.5),
+                    child: Image.asset("assets/am.png"),
+                  ),
+                   Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                         Text(
+                          "Emergency Stiuation",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: MediaQuery.of(context).size.width * 0.06,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          ),
+                          Text(
+                          "Call 1-0-9 an Emergency Stuation",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize:MediaQuery.of(context).size.width * 0.04
+                          ),
+                          ),
+                          Container(
+                            height: 30,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '1-0-9',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize:MediaQuery.of(context).size.width * 0.050
+                                ),
+                              ),
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
