03d8660dea73e2ac4822450e62303439a45af929
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-04-06 13:08:39 +0530

    add open street map

---
+++ lib/pages/map_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final  MapController _mapController = MapController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children:[
+          FlutterMap(
+            mapController: _mapController,
+            options:const  MapOptions(
+              initialCenter: LatLng(0, 0),
+              initialZoom: 2,
+              minZoom: 2,
+              maxZoom: 100,
+          ),
+           children:[
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            ),
+            CurrentLocationLayer()
+           ]
+          ),
+        ]
+      ),
+    );
+  }
+}
