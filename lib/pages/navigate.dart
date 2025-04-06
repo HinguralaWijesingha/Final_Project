7985c58971bf7558e7963fcc4dc87b7ba0c41c3f
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-04-06 13:33:08 +0530

    add location dependence

--- lib/pages/navigate.dart
+++ lib/pages/navigate.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:safe_pulse/pages/add_contact_page.dart';
 import 'package:safe_pulse/pages/home_page.dart';
-import 'package:safe_pulse/pages/map_page.dart';
+//import 'package:safe_pulse/pages/map_page.dart';
 import 'package:safe_pulse/pages/profile_page.dart';
 import 'package:safe_pulse/pages/chat_page.dart';
 //import 'package:safe_pulse/pages/setting.dart';
@@ -19,7 +19,7 @@ class _NavigateState extends State<Navigate> {
   int _selectedIndex = 0;
   final List<Widget> _pages = [
     const HomePage(),
-    const MapPage(),
+    //const MapPage(),
     const AddContactPage(),
     const ChatPage(),
     const ProfilePage(),
