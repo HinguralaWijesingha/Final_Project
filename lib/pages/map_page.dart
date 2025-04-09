ca518e8487b3b3c139a3bf0e7b555c08a2f9adad
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-04-09 23:23:27 +0530

    add user current location

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -12,39 +12,64 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  final  MapController _mapController = MapController();
+  final MapController _mapController = MapController();
+  //final Location _location = Location();
+ // final TextEditingController _locationController = TextEditingController();
+  //bool _isLoading = true;
+  LatLng? _currentLocation;
+  //LatLng? _destinationLocation;
+  //List<LatLng> _routeCoordinates = [];
+
+  Future<void> _userCurrentLocation() async {
+    if (_currentLocation != null) {
+      _mapController.move(_currentLocation!, 15);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Unable to get current location"),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children:[
-          FlutterMap(
+      body: Stack(children: [
+        FlutterMap(
             mapController: _mapController,
-            options:const  MapOptions(
-              initialCenter: LatLng(6.9271, 79.8612),
+            options:  MapOptions(
+              initialCenter: _currentLocation ?? LatLng(6.9271, 79.8612),
               initialZoom: 2,
               minZoom: 2,
               maxZoom: 100,
-          ),
-           children:[
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             ),
-            CurrentLocationLayer(
-              style: const LocationMarkerStyle(
-                marker: DefaultLocationMarker(
-                  child: Icon(
-                    Icons.location_pin,
-                    color: Colors.white,
-                  ),
-                ),
-                markerSize: Size(40, 40),
-                markerDirection: MarkerDirection.heading
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               ),
-            )
-           ]
-          ),
-        ]
+              CurrentLocationLayer(
+                style: const LocationMarkerStyle(
+                    marker: DefaultLocationMarker(
+                      child: Icon(
+                        Icons.location_pin,
+                        color: Colors.white,
+                      ),
+                    ),
+                    markerSize: Size(40, 40),
+                    markerDirection: MarkerDirection.heading),
+              )
+            ]),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        onPressed: _userCurrentLocation,
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.my_location,
+          size: 30,
+          color: Colors.white,
+        ),
       ),
     );
   }
