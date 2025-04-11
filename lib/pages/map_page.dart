cbf28dd14ff88458a2eddb04d7525f13ab0ff2b6
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-04-11 23:13:59 +0530

    create map and get location and route

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -13,32 +15,133 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   final MapController _mapController = MapController();
-  //final Location _location = Location();
-  //final TextEditingController _locationController = TextEditingController();
-  //bool _isLoading = true;
+  final TextEditingController _locationController = TextEditingController();
+  bool _isLoading = true;
   LatLng? _currentLocation;
-  //LatLng? _destinationLocation;
-  //List<LatLng> _routeCoordinates = [];
+  LatLng? _destinationLocation;
+  List<LatLng> _route = [];
 
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
 
+  Future<void> _getCurrentLocation() async {
+    if (!await _checkLocationPermission()) return;
 
-Future<void> _userCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _currentLocation = LatLng(position.latitude, position.longitude);
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _getDestinationLocation(String location) async {
+    final url = Uri.parse(
+        'https://nominatim.openstreetmap.org/search?q=$location&format=jsonv2&addressdetails=1');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      if (data.isNotEmpty) {
+        final lat = double.parse(data[0]['lat']);
+        final lon = double.parse(data[0]['lon']);
+        setState(() {
+          _destinationLocation = LatLng(lat, lon);
+        });
+        await _fetchRoute();
+      } else {
+        _showErrorMessage('Location not found');
+      }
+    } else {
+      _showErrorMessage('Failed to fetch location data');
+    }
+  }
+
+  Future<void> _fetchRoute() async {
+    if (_currentLocation == null || _destinationLocation == null) return;
+    final url = Uri.parse(
+      "http://router.project-osrm.org/route/v1/driving/"
+      '${_currentLocation!.longitude},${_currentLocation!.latitude};'
+      '${_destinationLocation!.longitude},${_destinationLocation!.latitude}?overview=full&geometries=polyline',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final geometry = data['routes'][0]['geometry'];
+      _decodePolyline(geometry);
+    } else {
+      _showErrorMessage('Failed to fetch route');
+    }
+  }
+
+  void _decodePolyline(String encodedPolyline) {
+    PolylinePoints polylinePoints = PolylinePoints();
+    List<PointLatLng> decodedPoints =
+        polylinePoints.decodePolyline(encodedPolyline);
+
+    setState(() {
+      _route = decodedPoints
+          .map((point) => LatLng(point.latitude, point.longitude))
+          .toList();
+    });
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      _showErrorMessage('Location services are disabled.');
+      return false;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        _showErrorMessage('Location permissions are denied');
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      _showErrorMessage('Location permissions are permanently denied');
+      return false;
+    }
+
+    return true;
+  }
+
+  void _userCurrentLocation() {
     if (_currentLocation != null) {
       _mapController.move(_currentLocation!, 15);
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text("Unable to get current location"),
-        ),
-      );
+      _showErrorMessage("Unable to get current location");
     }
+  }
+
+  void _showErrorMessage(String? message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message!),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        FlutterMap(
+      body:Stack(
+        children: [
+            _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        :
+          FlutterMap(
             mapController: _mapController,
             options: MapOptions(
               initialCenter: _currentLocation ?? LatLng(6.9271, 79.8612),
@@ -50,19 +153,77 @@ Future<void> _userCurrentLocation() async {
               TileLayer(
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               ),
-              CurrentLocationLayer(
-                style: const LocationMarkerStyle(
-                    marker: DefaultLocationMarker(
-                      child: Icon(
-                        Icons.location_pin,
-                        color: Colors.white,
-                      ),
+              if (_currentLocation != null)
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: _currentLocation!,
+                      width: 40,
+                      height: 40,
+                      child: const Icon(Icons.location_pin, color: Colors.red),
+                    )
+                  ],
+                ),
+              if (_destinationLocation != null)
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: _destinationLocation!,
+                      width: 40,
+                      height: 40,
+                      child: const Icon(Icons.flag, color: Colors.green),
+                    )
+                  ],
+                ),
+              if (_route.isNotEmpty)
+                PolylineLayer(
+                  polylines: [
+                    Polyline(
+                      points: _route,
+                      strokeWidth: 4.0,
+                      color: Colors.blue,
                     ),
-                    markerSize: Size(40, 40),
-                    markerDirection: MarkerDirection.heading),
-              )
-            ]),
-      ]),
+                  ],
+                )
+            ],
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(children: [
+                Expanded(
+                  child: TextField(
+                    controller: _locationController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Enter a location',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 16),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    final location = _locationController.text.trim();
+                    if (location.isNotEmpty) {
+                      _getDestinationLocation(location);
+                    }
+                  },
+                  icon: const Icon(Icons.search),
+                ),
+              ]),
+            ),
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         elevation: 0,
         onPressed: _userCurrentLocation,
