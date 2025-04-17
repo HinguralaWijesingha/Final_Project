70e8dd2e6538a6158f126d0ac06cf69f1d9ad966
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-04-18 00:41:03 +0530

    add new text

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -322,7 +322,7 @@ class _MapPageState extends State<MapPage> {
         children: [
           FloatingActionButton.extended(
             onPressed: _shareLocationWithContact,
-            label: const Text("Share"),
+            label: const Text(""),
             icon: const Icon(Icons.share),
             backgroundColor: Colors.green,
           ),
