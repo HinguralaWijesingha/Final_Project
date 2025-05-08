b85f6e4fcf1b77810732838e5c212a3252330c61
Hingurala Wijesingha <anjithaimalshan08@gmail.com>
2025-05-09 01:55:12 +0530

    clean the code

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    //await SMSService().initialize(); 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
